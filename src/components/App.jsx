a1c607feed785a68276c785c770ac01167b18dec
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-06-28 22:30:03 +0300

    update index.js and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,17 @@
-import { useState } from 'react';
 import Layout from './Layout/Layout';
-import FormList from './FormList/FormList';
-import initialContacts from './data/contacts.json';
-import useLocalStorage from './hook/useLocalStorage';
 import MainTitle from './Layout/Title';
+import FormList from './FormList/FormList';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import { nanoid } from 'nanoid';
-import Notiflix from 'notiflix';
 
 function App() {
-  const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
-  const [filter, setFilter] = useState('');
-
-  const addContact = ({ name, number }) => {
-    const normalizedName = name.toLowerCase();
-    const isAdded = contacts.find(
-      el => el.name.toLowerCase() === normalizedName
-    );
-
-    if (isAdded) {
-      alert(`${name}: is already in contacts`);
-      Notiflix.Notify.failure(`${name}: is already in contacts`);
-      return;
-    }
-
-    const contact = {
-      id: nanoid(),
-      name: name,
-      number: number,
-    };
-
-    setContacts(prevState => [...prevState, contact]);
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-  const changeFilter = e => {
-    setFilter(e.currentTarget.value);
-  };
-
-  const deleteContacts = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId)
-    );
-  };
-
-  const visibleContacts = getVisibleContacts();
   return (
     <Layout>
       <MainTitle title="Phonebook" />
-      <FormList onSubmit={addContact} />
+      <FormList />
       <MainTitle title="Contacts" />
-      <Filter value={filter} onChange={changeFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContacts} />
+      <Filter />
+      <ContactList />
     </Layout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </PersistGate>
   </React.StrictMode>
 );
