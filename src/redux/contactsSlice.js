864ffc139f9b46584bfb907f424033d16b53fe60
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-07-01 15:06:17 +0300

    update

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,10 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { contactsDefault } from 'data/contactsDefault';
 
 const initialState = {
-  list: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  list: contactsDefault,
 };
 
 const contactsSlice = createSlice({
@@ -17,7 +13,7 @@ const contactsSlice = createSlice({
   reducers: {
     addContact: {
       reducer(state, action) {
-         state.list = [...state.list, action.payload]; //^ Додавання нового контакта
+        state.list = [...state.list, action.payload]; //^ Додавання нового контакта
       },
     },
     prepare: (name, number) => {
@@ -30,7 +26,7 @@ const contactsSlice = createSlice({
         },
       };
     },
-    deleteContacts(state, action)  {
+    deleteContacts(state, action) {
       state.list = state.list.filter(contact => contact.id !== action.payload);
     },
   },
